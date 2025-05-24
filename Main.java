07d88680e15b67d58af36c3f01860d68845ed74c
Jujo1234 <jorgeberenguer06@gmail.com>
2025-05-24 15:43:23 +0200

    Primer cambio

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        double precioProducto1 = 10.5;
+        double[] precios = {10.5, 25.3};
+        double total = calcularPrecioTotal(precios);
+
+        System.out.println("El precio total es: " + total);
         double precioProducto2 = 25.3;
 
         double total = calcularPrecioTotal(precioProducto1, precioProducto2);
